3785ececd18e82bb63ab048ea4dad75187431b32
sri942 <106247385+sri942@users.noreply.github.com>
2022-06-28 17:30:53 +0530

    Add files via upload

---
+++ forloop.ts
@@ -0,0 +1,15 @@
+for(let i=0;i<4;i++){
+    console.log("element"+i);
+}
+let array=[16,15,3,9,4];
+for(let index=0;index<array.length;index++){
+    const element=array[index];
+    console.log(element);
+}
+let list = [7, 3, 8];
+for (let i in list) {
+  console.log(i); 
+}
+for (let i of list) {
+  console.log(i); 
+}
---
+++ whileloop.ts
@@ -0,0 +1,13 @@
+var i:number=0;
+var user:string[]=["neha","ravi","sasi","john"];
+while(user[i]){
+    console.log("user:",user[i]);
+    i++;
+}
+//printing user names i  reverse order
+var i:number=3;
+var user:string[]=["neha","ravi","sasi","john"];
+while(user[i]){
+    console.log("user:",user[i]);
+    i--;
+}
