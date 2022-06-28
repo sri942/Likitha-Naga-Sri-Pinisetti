dc9c5d73ba468c3ff960ed6850522ee03ef6fc04
sri942 <106247385+sri942@users.noreply.github.com>
2022-06-28 17:31:57 +0530

    Add files via upload

---
+++ tsobject.ts
@@ -0,0 +1,8 @@
+class Greeting{
+    greet():void{
+        console.log("good morning everyone")
+    }
+}
+var object=new Greeting();            //object creation
+object.greet();
+console.log(object);
