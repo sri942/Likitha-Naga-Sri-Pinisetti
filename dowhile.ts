9f859a2144b137f51d00430f1b5a8ac36129a0c0
sri942 <106247385+sri942@users.noreply.github.com>
2022-06-28 19:20:08 +0530

    Add files via upload

---
+++ dowhile.ts
@@ -0,0 +1,25 @@
+for(let i=1;i<=5;i++){
+    console.log(i);
+    if(i==3){
+        continue;
+    }
+    else
+    {
+        break;
+    }
+}
+
+var a:number=3;
+ while(a<7)
+ {
+     console.log(a);
+     a++
+ }
+ var b:number=1;
+ do{
+     console.log(b);
+     b++;
+     if(b == 5){
+         break;
+     }
+ }while(b<8)
