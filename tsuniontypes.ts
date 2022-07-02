7a77f149401823c3b4a9abc5b3a4b832eac1de57
sri942 <106247385+sri942@users.noreply.github.com>
2022-07-02 14:59:08 +0530

    Add files via upload

---
+++ tsuniontypes.ts
@@ -0,0 +1,9 @@
+function printStatusCode(code:string|number)
+{ 
+    console.log(`my status code is ${code}.`)
+
+}
+printStatusCode(920);
+printStatusCode('920');
+printStatusCode("red");
+printStatusCode(null);
